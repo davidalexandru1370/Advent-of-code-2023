29db7c479ebb527b9563adcb51c2c3ec679d1ce9
David Alexandru <fanefaraon696@gmail.com>
2023-12-15 20:08:52 +0200

    added: day 15 solution part 2

--- aoc2023/aoc2023/src/main/kotlin/day15/Day15.kt
+++ aoc2023/aoc2023/src/main/kotlin/day15/Day15.kt
@@ -2,9 +2,12 @@ package day15
 
 import java.io.File
 
+data class Lens(var name: String, var focal: Int)
+
 class Day15 {
     init {
-        solve1()
+        //solve1()
+        solve2()
     }
 
     private fun hash(string: String): Int {
@@ -17,8 +20,49 @@ class Day15 {
         return sum
     }
 
+
+    private fun solve2() {
+        val boxes: HashMap<Int, List<Lens>> = HashMap()
+        for (x in 0..255) {
+            boxes[x] = ArrayList()
+        }
+
+        File("src/main/kotlin/day15/1.txt")
+            .readLines()
+            .first
+            .split(",")
+            .forEach {
+                val name: String = Regex("[a-z]+").find(it)!!.value
+                val hashedValue: Int = hash(name)
+                if (it.contains("=")) {
+                    val newFocal: Int = it.split("=")[1].toInt()
+                    val existingBoxes: List<Lens> = boxes[hashedValue]!!.filter { box -> box.name == name }
+                    if (existingBoxes.isNotEmpty()) {
+                        existingBoxes.forEach { box ->
+                            box.focal = newFocal
+                        }
+                    } else {
+                        boxes[hashedValue]!!.addLast(Lens(name, newFocal))
+                    }
+                } else {
+                    boxes[hashedValue] = boxes[hashedValue]!!.filter { box -> box.name != name }
+                }
+            }
+        var sum: Long = 0L
+
+        for (boxNumber in 0..255) {
+            boxes[boxNumber]!!.forEachIndexed { index, box ->
+                sum += (1 + boxNumber) * (index + 1) * box.focal * 1L
+            }
+        }
+
+        println(sum)
+
+
+    }
+
     private fun solve1() {
-        var result: Int =
+        val result: Int =
             File("src/main/kotlin/day15/1.txt")
                 .readLines()
                 .first
